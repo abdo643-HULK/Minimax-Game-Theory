a237d4c10e5d523d4a26a675b49a13f900555e12
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-06-22 09:33:51 +0200

    cleaned up

--- code/src/lib/core/algorithms/alphaBeta.ts
+++ code/src/lib/core/algorithms/alphaBeta.ts
@@ -1,11 +1,7 @@
-import { getPlayableColumns, getPlayableRow, getValidLocations } from '../getValidLocation';
-
-import { State } from '../State';
 import { Cell } from '../entities';
 import { getActions } from '../getActions';
-import { checkForWin, winningMove } from '../checkForWin';
 
-import type { IBoard } from '$lib/core';
+import type { State } from '../State';
 
 export function alphaBeta(state: State, depth: number): Move | undefined {
 	return undefined;
