0ecebbcef36d792cf5c5264d43240e5749bdadc6
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-06-22 13:10:54 +0200

    algo done

--- code/src/lib/core/algorithms/minimax.ts
+++ code/src/lib/core/algorithms/minimax.ts
@@ -1,4 +1,6 @@
 import { Cell } from '../entities';
+import { getActions } from '../getActions';
+import { result } from '../result';
 
 import type { State } from '../State';
 
@@ -8,5 +10,55 @@ import type { State } from '../State';
 // https://towardsdatascience.com/creating-the-perfect-connect-four-ai-bot-c165115557b0
 
 export function minimax(state: State, depth = Infinity): Move | undefined {
-	return undefined;
+	const player = state.player;
+
+	function checkIsTerminal(state: State, currentDepth: number): boolean {
+		return currentDepth === 0 || state.utility !== 0 || state.moves.length === 0;
+	}
+
+	function evalFn(state: State): number {
+		return player === Cell.PLAYER ? state.utility : -state.utility;
+	}
+
+	function maximizer(state: State, depth: number) {
+		let best = -Infinity;
+		if (checkIsTerminal(state, depth)) return evalFn(state);
+
+		for (const action of getActions(state)) {
+			const [row, col] = action;
+			best = Math.max(best, minimizer(result(state, action), depth - 1));
+			state.board[row][col] = Cell.EMPTY;
+		}
+
+		return best;
+	}
+
+	function minimizer(state: State, depth: number): number {
+		let best = Infinity;
+		if (checkIsTerminal(state, depth)) return evalFn(state);
+
+		for (const action of getActions(state)) {
+			const [row, col] = action;
+			best = Math.min(best, maximizer(result(state, action), depth - 1));
+			state.board[row][col] = Cell.EMPTY;
+		}
+
+		return best;
+	}
+
+	let best = Infinity;
+	let bestMove: Move | undefined = undefined;
+
+	for (const move of getActions(state)) {
+		const [row, col] = move;
+		const value = minimizer(result(state, move), depth - 1);
+		state.board[row][col] = Cell.EMPTY;
+
+		if (value > best) {
+			best = value;
+			bestMove = move;
+		}
+	}
+
+	return bestMove;
 }
