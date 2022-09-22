4cdabccb08c516402988099ec4cf88f7c320fef6
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2022-09-22 21:04:07 +0200

    fixed versioning

---
+++ code/static/AI.py
@@ -0,0 +1,244 @@
+import copy
+import itertools
+import random
+from collections import namedtuple
+
+import numpy as np
+
+GameState = namedtuple('GameState', 'to_move, utility, board, moves')
+idx = 0
+def minmax_decision(state, game):
+    """Given a state in a game, calculate the best move by searching
+    forward all the way to the terminal states. [Figure 5.3]"""
+
+    player = game.to_move(state)
+
+    def max_value(state):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a)))
+        return v
+
+    def min_value(state):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = np.inf
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a)))
+        return v
+
+    # Body of minmax_decision:
+    res = max(game.actions(state), key=lambda a: min_value(game.result(state, a)))
+    print(res)
+    return res
+
+def alpha_beta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None):
+    """Search game to determine best action; use alpha-beta pruning.
+    This version cuts off search and uses an evaluation function."""
+    global idx 
+
+    player = game.to_move(state)
+
+    # Functions used by alpha_beta
+    def max_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            # print("max_value eval_fn",player,  state.utility)
+            return eval_fn(state)
+        v = -np.inf
+        # print("max_value", game.actions(state))
+        for a in game.actions(state):
+            # print("max_value before", state.to_move)
+            res = game.result(state, a)
+            # print("max_value", state.to_move, res.utility)
+            v = max(v, min_value(res, alpha, beta, depth + 1))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    def min_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            # print("min_value eval_fn", player, state.utility)
+            return eval_fn(state)
+        v = np.inf
+        # print("min_value", game.actions(state))
+        for a in game.actions(state):
+            # print("min_value before", state.to_move)
+            res = game.result(state, a)
+            # print("min_value", state.to_move, res.utility)
+            v = min(v, max_value(res, alpha, beta, depth + 1))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alpha_beta_cutoff_search starts here:
+    # The default test cuts off at depth d or at a terminal state
+    cutoff_test = (cutoff_test or (lambda state, depth: depth > d or game.terminal_test(state)))
+    eval_fn = eval_fn or (lambda state: game.utility(state, player))
+    best_score = -np.inf
+    # beta = np.inf
+    best_action = None
+    print("\n")
+    # print(state.board)
+    # print(game.actions(state))
+    for a in game.actions(state):
+        res = game.result(state, a)
+        # print(res.utility)
+        v = min_value(res, best_score, np.inf, 1)
+        # print(a, v)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    idx += 1
+    # print(best_action)
+    return best_action
+
+
+class Game:
+    """A game is similar to a problem, but it has a utility for each
+    state and a terminal test instead of a path cost and a goal
+    test. To create a game, subclass this class and implement actions,
+    result, utility, and terminal_test. You may override display and
+    successors or you can inherit their default methods. You will also
+    need to set the .initial attribute to the initial state; this can
+    be done in the constructor."""
+
+    def actions(self, state):
+        """Return a list of the allowable moves at this point."""
+        raise NotImplementedError
+
+    def result(self, state, move):
+        """Return the state that results from making a move from a state."""
+        raise NotImplementedError
+
+    def utility(self, state, player):
+        """Return the value of this final state to player."""
+        raise NotImplementedError
+
+    def terminal_test(self, state):
+        """Return True if this is a final state for the game."""
+        return not self.actions(state)
+
+    def to_move(self, state):
+        """Return the player whose move it is in this state."""
+        return state.to_move
+
+    def display(self, state):
+        """Print or otherwise display the state."""
+        print(state)
+
+    def __repr__(self):
+        return '<{}>'.format(self.__class__.__name__)
+
+    def play_game(self, *players):
+        """Play an n-person, move-alternating game."""
+        state = self.initial
+        while True:
+            for player in players:
+                move = player(self, state)
+                state = self.result(state, move)
+                self.display(state)
+                if self.terminal_test(state):
+                    self.display(state)
+                    return self.utility(state, self.to_move(self.initial))
+
+class TicTacToe(Game):
+    """Play TicTacToe on an h x v board, with Max (first player) playing 'X'.
+    A state has the player to move, a cached utility, a list of moves in
+    the form of a list of (x, y) positions, and a board, in the form of
+    a dict of {(x, y): Player} entries, where Player is 'X' or 'O'."""
+
+    def __init__(self, h=3, v=3, k=3):
+        self.h = h
+        self.v = v
+        self.k = k
+        moves = [(x, y) for x in range(1, h + 1)
+                 for y in range(1, v + 1)]
+        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
+
+    def actions(self, state):
+        """Legal moves are any square not yet taken."""
+        return state.moves
+
+    def result(self, state, move):
+        if move not in state.moves:
+            return state  # Illegal move has no effect
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
+                         utility=self.compute_utility(board, move, state.to_move),
+                         board=board, moves=moves)
+
+    def utility(self, state, player):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player == 'X' else -state.utility
+
+    def terminal_test(self, state):
+        """A state is terminal if it is won or there are no empty squares."""
+        return state.utility != 0 or len(state.moves) == 0
+
+    def display(self, state):
+        board = state.board
+        for x in range(1, self.h + 1):
+            for y in range(1, self.v + 1):
+                print(board.get((x, y), '.'), end=' ')
+            print()
+
+    def compute_utility(self, board, move, player):
+        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
+        if (self.k_in_row(board, move, player, (0, 1)) or
+                self.k_in_row(board, move, player, (1, 0)) or
+                self.k_in_row(board, move, player, (1, -1)) or
+                self.k_in_row(board, move, player, (1, 1))):
+            return +1 if player == 'X' else -1
+        else:
+            return 0
+        
+
+    def k_in_row(self, board, move, player, delta_x_y):
+        """Return true if there is a line through move on board for player."""
+        (delta_x, delta_y) = delta_x_y
+        x, y = move
+        n = 0  # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        
+        n -= 1  # Because we counted move itself twice
+        return n >= self.k
+
+
+class ConnectFour(TicTacToe):
+    """A TicTacToe-like game in which you can only make a move on the bottom
+    row, or in a square directly above an occupied square.  Traditionally
+    played on a 7x6 board and requiring 4 in a row."""
+
+    def __init__(self, h=6, v=7, k=4):
+        TicTacToe.__init__(self, h, v, k)
+
+    def actions(self, state):
+        actionsRes = [(x, y) for (x, y) in state.moves
+                if x == self.h or (x + 1 , y ) in state.board]
+        # print(actionsRes)
+        return actionsRes
+
+def alpha_beta(game, state):
+    return alpha_beta_cutoff_search(state, game)
+
+def minmax(game, state):
+    return minmax_decision(state, game)
+
+def random_player(game, state):
+    """A player that chooses a legal move at random."""
+    return random.choice(game.actions(state)) if game.actions(state) else None
+
+print(ConnectFour().play_game(minmax, alpha_beta))
